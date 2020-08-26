2d8e1f25007eff1bdeaeda076f959ecbc9bf0de0
Matt Retzer <7025870+mattretzer@users.noreply.github.com>
2020-08-26 16:20:36 -0400

    updating stuff for Windows

--- _gradle-install/checkWord.bat
+++ _gradle-install/checkWord.bat
@@ -1,8 +1,8 @@
-@echo off 
+@echo off
 SETLOCAL EnableExtensions
 set EXE=WINWORD.EXE
 FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF %%x == %EXE% goto FOUND
-FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO echo %%x
+FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO echo %%x > nul
 echo not running
 goto FIN
 :FOUND
